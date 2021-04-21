59832403556ee397dbd659c19de24bf1cf76323b
Swung0x48 <swung0x48@outlook.com>
2021-04-21 10:28:10 +0800

    Data structure for multiple peer balls

--- BallanceMMOClient/BallanceMMOClient.cpp
+++ BallanceMMOClient/BallanceMMOClient.cpp
@@ -17,24 +17,26 @@ void BallanceMMOClient::OnPostStartMenu() {
 void BallanceMMOClient::OnProcess()
 {
 	if (m_bml->IsPlaying()) {
-		auto* ball = get_current_ball();
+		auto ball = get_current_ball();
 		if (strcmp(ball->GetName(), player_ball_->GetName()) != 0) {
 			// OnTrafo
-			spirit_ball_ = spirit_balls_[ball->GetName()].obj;
+			spirit_ball_ = template_balls_[ball_name_to_idx_[ball->GetName()[0]]];
+			
 			VxVector vec;
 			ball->GetPosition(&vec);
 			spirit_ball_->SetPosition(vec);
 			spirit_ball_->Show(CKSHOW);
-			//
-
+			
+			// Update current player ball
 			player_ball_ = ball;
+			ball_status_.type = ball_name_to_idx_[player_ball_->GetName()[0]];
 		}
 
-		player_ball_->GetPosition(&position_);
-		player_ball_->GetQuaternion(&rotation_);
+		player_ball_->GetPosition(&ball_status_.position);
+		player_ball_->GetQuaternion(&ball_status_.rotation);
 		msg_.clear();
 		msg_.header.id = MsgType::MessageAll;
-		msg_ << position_ << rotation_;
+		msg_ << ball_status_;
 		client_.broadcast_message(msg_);
 	}
 }
@@ -42,10 +44,21 @@ void BallanceMMOClient::OnProcess()
 void BallanceMMOClient::OnStartLevel()
 {
 	player_ball_ = get_current_ball();
-	spirit_ball_ = spirit_balls_[player_ball_->GetName()].obj;
+	spirit_ball_ = template_balls_[ball_name_to_idx_[player_ball_->GetName()[0]]];
 	VxVector vec(42, 15, -153);
 	spirit_ball_->SetPosition(vec);
 	spirit_ball_->Show(CKSHOW);
+
+	msg_receive_thread_ = std::thread([this]() {
+		while (m_bml->IsIngame())
+		{
+			while (client_.get_incoming_messages().empty())
+				client_.get_incoming_messages().wait();
+
+			auto msg = client_.get_incoming_messages().pop_front();
+			process_incoming_message(msg);
+		}
+	});
 }
 
 void BallanceMMOClient::OnUnload()
@@ -58,12 +71,12 @@ void BallanceMMOClient::OnLoadObject(CKSTRING filename, BOOL isMap, CKSTRING mas
 	if (strcmp(filename, "3D Entities\\Balls.nmo") == 0) {
 		CKDataArray* physicalized_ball = m_bml->GetArrayByName("Physicalize_GameBall");
 		for (int i = 0; i < physicalized_ball->GetRowCount(); i++) {
-			SpiritBall ball;
+			CK3dObject* ball;
 			std::string ball_name;
 			ball_name.resize(physicalized_ball->GetElementStringValue(i, 0, nullptr), '\0');
 			physicalized_ball->GetElementStringValue(i, 0, &ball_name[0]);
 			ball_name.pop_back();
-			ball.obj = m_bml->Get3dObjectByName(ball_name.c_str());
+			ball = m_bml->Get3dObjectByName(ball_name.c_str());
 
 			CKDependencies dep;
 			dep.Resize(40); dep.Fill(0);
@@ -71,9 +84,9 @@ void BallanceMMOClient::OnLoadObject(CKSTRING filename, BOOL isMap, CKSTRING mas
 			dep[CKCID_OBJECT] = CK_DEPENDENCIES_COPY_OBJECT_NAME | CK_DEPENDENCIES_COPY_OBJECT_UNIQUENAME;
 			dep[CKCID_MESH] = CK_DEPENDENCIES_COPY_MESH_MATERIAL;
 			dep[CKCID_3DENTITY] = CK_DEPENDENCIES_COPY_3DENTITY_MESH;
-			ball.obj = static_cast<CK3dObject*>(m_bml->GetCKContext()->CopyObject(ball.obj, &dep, "_Spirit"));
-			for (int j = 0; j < ball.obj->GetMeshCount(); j++) {
-				CKMesh* mesh = ball.obj->GetMesh(j);
+			ball = static_cast<CK3dObject*>(m_bml->GetCKContext()->CopyObject(ball, &dep, "_Spirit_"));
+			for (int j = 0; j < ball->GetMeshCount(); j++) {
+				CKMesh* mesh = ball->GetMesh(j);
 				for (int k = 0; k < mesh->GetMaterialCount(); k++) {
 					CKMaterial* mat = mesh->GetMaterial(k);
 					mat->EnableAlphaBlend();
@@ -82,11 +95,12 @@ void BallanceMMOClient::OnLoadObject(CKSTRING filename, BOOL isMap, CKSTRING mas
 					VxColor color = mat->GetDiffuse();
 					color.a = 0.5f;
 					mat->SetDiffuse(color);
-					ball.materials.push_back(mat);
+					//ball.materials.push_back(mat);
 					m_bml->SetIC(mat);
 				}
 			}
-			spirit_balls_[ball_name] = ball;
+			template_balls_[i] = ball;
+			ball_name_to_idx_[ball_name[0]] = i;
 		}
 	}
 
@@ -95,7 +109,50 @@ void BallanceMMOClient::OnLoadObject(CKSTRING filename, BOOL isMap, CKSTRING mas
 	}
 }
 
-void BallanceMMOClient::process_incoming_message(const blcl::net::owned_message<MsgType>& msg)
+void BallanceMMOClient::process_incoming_message(blcl::net::owned_message<MsgType>& msg)
 {
+	switch (msg.msg.header.id) {
+		case MsgType::MessageAll: {
+			BallStatus status;
+			int ball_index;
+			msg.msg >> ball_index >> status;
+			uint32_t remote_id = msg.remote->get_id();
+			if (peer_balls_.find(remote_id) == peer_balls_.end()) {
+				// if incoming msg from a new client, then init balls and set IC
+				PeerState state;
+				for (size_t i = 0; i < 3; i++)
+					state.balls[i] = init_spirit_ball(i, remote_id);
+				state.current_ball = ball_index;
+				peer_balls_[remote_id] = state;
+			}
 
+			peer_balls_[remote_id].balls[ball_index]->SetPosition(status.position);
+			peer_balls_[remote_id].balls[ball_index]->SetQuaternion(status.rotation);
+			break;
+		}
+	}
+}
+
+CK3dObject* BallanceMMOClient::init_spirit_ball(int ball_index, uint32_t id) {
+	CKDependencies dep;
+	dep.Resize(40); dep.Fill(0);
+	dep.m_Flags = CK_DEPENDENCIES_CUSTOM;
+	dep[CKCID_OBJECT] = CK_DEPENDENCIES_COPY_OBJECT_NAME | CK_DEPENDENCIES_COPY_OBJECT_UNIQUENAME;
+	dep[CKCID_MESH] = CK_DEPENDENCIES_COPY_MESH_MATERIAL;
+	dep[CKCID_3DENTITY] = CK_DEPENDENCIES_COPY_3DENTITY_MESH;
+	CK3dObject* ball = static_cast<CK3dObject*>(m_bml->GetCKContext()->CopyObject(template_balls_[ball_index], &dep, std::to_string(id).c_str()));
+	for (int j = 0; j < ball->GetMeshCount(); j++) {
+		CKMesh* mesh = ball->GetMesh(j);
+		for (int k = 0; k < mesh->GetMaterialCount(); k++) {
+			CKMaterial* mat = mesh->GetMaterial(k);
+			//mat->EnableAlphaBlend();
+			//mat->SetSourceBlend(VXBLEND_SRCALPHA);
+			//mat->SetDestBlend(VXBLEND_INVSRCALPHA);
+			//VxColor color = mat->GetDiffuse();
+			//color.a = 0.5f;
+			//mat->SetDiffuse(color);
+			m_bml->SetIC(mat);
+		}
+	}
+	return ball;
 }
--- BallanceMMOClient/BallanceMMOClient.h
+++ BallanceMMOClient/BallanceMMOClient.h
@@ -17,24 +17,37 @@ public:
 	virtual CKSTRING GetAuthor() override { return "Swung0x48"; }
 	virtual CKSTRING GetDescription() override { return "The client to connect your game to the universe."; }
 	DECLARE_BML_VERSION;
-
+	
 private:
-	struct SpiritBall {
-		CK3dObject* obj;
-		std::vector<CKMaterial*> materials;
+	//struct SpiritBall {
+		//CK3dObject* obj = nullptr;
+		//std::vector<CKMaterial*> materials;
+	//};
+
+	struct BallStatus {
+		int type;
+		VxVector position;
+		VxQuaternion rotation;
 	};
 
 	const size_t MSG_MAX_SIZE = 500;
 	Client client_;
-	std::thread msg_thread_;
+	std::thread msg_receive_thread_;
 	blcl::net::message<MsgType> msg_ = blcl::net::message<MsgType>();
-	blcl::net::tsqueue<blcl::net::message<MsgType>> msg_queue_;
 	CK3dObject* player_ball_ = nullptr;
 	CK3dObject* spirit_ball_ = nullptr;
-	VxVector position_;
-	VxQuaternion rotation_;
-	CKDataArray* current_level_array_;
-	std::unordered_map<std::string, SpiritBall> spirit_balls_;
+	BallStatus ball_status_;
+	//VxVector position_;
+	//VxQuaternion rotation_;
+	CKDataArray* current_level_array_ = nullptr;
+	std::unordered_map<char, int> ball_name_to_idx_;
+	CK3dObject* template_balls_[3];
+
+	struct PeerState {
+		CK3dObject* balls[3];
+		int current_ball = 0;
+	};
+	std::unordered_map<uint32_t, PeerState> peer_balls_;
 
 	virtual void OnLoad() override;
 	virtual void OnPostStartMenu() override;
@@ -45,6 +58,7 @@ private:
 	virtual void OnStartLevel() override;
 	virtual void OnUnload() override;
 
+private:
 	CK3dObject* get_current_ball() { 
 		if (current_level_array_)
 			return static_cast<CK3dObject*>(current_level_array_->GetElementObject(0, 1));
@@ -52,5 +66,6 @@ private:
 		return nullptr;
 	}
 
-	void process_incoming_message(const blcl::net::owned_message<MsgType>& msg);
+	void process_incoming_message(blcl::net::owned_message<MsgType>& msg);
+	CK3dObject* init_spirit_ball(int ball_index, uint32_t id);
 };
