d37ba4b0d27f9cd3f27d07b042060eeddde30d5b
Swung0x48 <swung0x48@outlook.com>
2021-07-21 23:18:19 +0800

    Refactored client

--- BallanceMMOServer/client.cpp
+++ BallanceMMOServer/client.cpp
@@ -11,14 +11,59 @@ public:
         send(msg);
     }
 
-    void disconnect() {
+    void disconnect() override {
         ammo::common::message<PacketType> msg;
         msg.header.id = ClientDisconnect;
         for (int i = 0; i < 10; ++i) {
             send(msg);
         }
+        ammo::role::client<PacketType>::disconnect();
     }
+
     std::string name = "Spectator001";
+protected:
+    void on_message(ammo::common::owned_message<PacketType> &msg) override {
+        if (!connected()) {
+            if (msg.message.header.id == ConnectionAccepted) {
+                confirm_validation();
+                std::cout << "[INFO] Accepted by server!" << std::endl;
+            } else if (msg.message.header.id == ConnectionChallenge) {
+                uint64_t checksum;
+                msg.message >> checksum;
+                checksum = encode_for_validation(checksum);
+                msg.message.clear();
+                msg.message << checksum;
+                ammo::entity::string<PacketType> str = this->name;
+                str.serialize(msg.message);
+                msg.message.header.id = ConnectionResponse;
+                send(msg.message);
+            } else if (msg.message.header.id == Denied) {
+                std::cout << "[WARN] Rejected by server." << std::endl;
+            }
+        } else {
+            switch (msg.message.header.id) {
+                case PacketFragment: {
+                    break;
+                }
+                case Denied: {
+                    break;
+                }
+                case Ping: {
+                    auto now = std::chrono::system_clock::now().time_since_epoch().count();
+                    uint64_t then; msg.message >> then;
+                    auto ping = now - then; // in microseconds
+                    std::cout << "[INFO] Ping: " << ping / 1000 << " ms" << std::endl;
+                    break;
+                }
+                case GameState: {
+                    break;
+                }
+                default: {
+                    std::cout << "[WARN] Unknown message ID: " << msg.message.header.id << std::endl;
+                }
+            }
+        }
+    }
 };
 
 int main() {
@@ -29,61 +74,6 @@ int main() {
         std::cout << "[INFO] Connection request sent to server. Waiting for reply..." << std::endl;
     }
 
-    std::atomic_bool will_quit = false;
-    std::thread update_thread([&]() {
-        while (!will_quit) {
-            while (client.get_state() == ammo::role::client_state::Pending) {
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                if (client.get_incoming_messages().empty())
-                    client.send_request();
-                else {
-                    auto msg = client.get_incoming_messages().pop_front();
-                    if (msg.message.header.id == ConnectionAccepted) {
-                        client.confirm_validation();
-                        std::cout << "[INFO] Accepted by server!" << std::endl;
-                    } else if (msg.message.header.id == ConnectionChallenge) {
-                        uint64_t checksum; msg.message >> checksum;
-                        checksum = encode_for_validation(checksum);
-                        msg.message.clear();
-                        msg.message << checksum;
-                        ammo::entity::string<PacketType> name = client.name;
-                        name.serialize(msg.message);
-                        msg.message.header.id = ConnectionResponse;
-                        client.send(msg.message);
-                    } else if (msg.message.header.id == Denied) {
-                        std::cout << "[WARN] Rejected by server." << std::endl;
-                    }
-                }
-            }
-            if (client.connected()) {
-                if (!client.get_incoming_messages().empty()) {
-                    auto msg = client.get_incoming_messages().pop_front().message;
-                    switch (msg.header.id) {
-                        case PacketFragment: {
-                            break;
-                        }
-                        case Denied: {
-                            break;
-                        }
-                        case Ping: {
-                            auto now = std::chrono::system_clock::now().time_since_epoch().count();
-                            uint64_t then; msg >> then;
-                            auto ping = now - then; // in microseconds
-                            std::cout << "[INFO] Ping: " << ping / 1000 << " ms" << std::endl;
-                            break;
-                        }
-                        case GameState: {
-                            break;
-                        }
-                        default: {
-                            std::cout << "[WARN] Unknown message ID: " << msg.header.id << std::endl;
-                        }
-                    }
-                }
-            }
-        }
-    });
-
     while (true) {
         int a; std::cin >> a;
         if (a == 1) {
@@ -93,12 +83,9 @@ int main() {
             msg << now;
             client.send(msg);
         } else if (a == 2) {
-            will_quit = true;
+            client.disconnect();
+            client.shutdown();
             break;
         }
     }
-    client.disconnect();
-    client.shutdown();
-    if (update_thread.joinable())
-        update_thread.join();
 }
