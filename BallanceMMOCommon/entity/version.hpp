715b58153d986e56c9817894fb350fbd29951286
Swung0x48 <swung0x48@outlook.com>
2022-04-16 18:34:49 +0800

    fix: bump up version

--- BallanceMMOCommon/entity/version.hpp
+++ BallanceMMOCommon/entity/version.hpp
@@ -16,7 +16,7 @@ namespace bmmo {
         uint8_t minor = 1;
         uint8_t subminor = 2;
         stage_t stage = Alpha;
-        uint8_t build = 5;
+        uint8_t build = 7;
 
         std::string to_string() {
             std::string stage_s = "";
