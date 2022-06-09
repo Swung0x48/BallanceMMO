f45edc55b35716e9298f1868ae95afcb771c701f
Xenapte <17661805+Xenapte@users.noreply.github.com>
2022-06-09 12:08:07 +0800

    fix: prevent infinite quick loops

--- BallanceMMOServer/start_ballancemmo_loop.sh
+++ BallanceMMOServer/start_ballancemmo_loop.sh
@@ -13,10 +13,12 @@ for (( ; ; ))
 do
     set -o pipefail
     log_name=$(date +"log_%Y%m%d%H%M.log")
+    chmod +x ./BallanceMMOServer
+    SECONDS=0
     ./BallanceMMOServer "$@" 2>&1 | tee $log_name
     server_status=$?
     archive_logs
-    if [ $server_status -eq 0 ] || [ $server_status -eq 1 ]
+    if [ $server_status -eq 0 ] || [ $server_status -eq 1 ] || [ $SECONDS -lt 10 ]
     then
         break
     fi
