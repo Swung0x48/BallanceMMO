28e6e8c3eb8d1841677e5651576107c1bd211208
Swung0x48 <swung0x48@outlook.com>
2022-06-21 20:34:37 +0800

    fix(workflow): exclude unnecessary garbage

--- BallanceMMOServer/postbuild.bat
+++ BallanceMMOServer/postbuild.bat
@@ -1,6 +1,5 @@
 @echo off
 set type=%1
-copy bin/%type%/*.dll %type%/ -Y
-copy _deps/yaml-cpp-build/%type%/*.dll %type%/ -Y
-copy bmmo_loop.ps1 %type%/ -Y
-copy start_ballancemmo_loop.bat %type%/ -Y
+copy _deps\yaml-cpp-build\%type%\*.dll %type%\ /Y
+copy bmmo_loop.ps1 %type%\ /Y
+copy start_ballancemmo_loop.bat %type%\ /Y
