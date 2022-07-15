17ee335467dfd598ea2ffe996463726675e57c51
Xenapte <17661805+Xenapte@users.noreply.github.com>
2022-07-15 11:43:07 +0800

    fix: bump version

--- BallanceMMOCommon/entity/version.hpp
+++ BallanceMMOCommon/entity/version.hpp
@@ -16,7 +16,7 @@ namespace bmmo {
         uint8_t minor = 2;
         uint8_t subminor = 8;
         stage_t stage = Alpha;
-        uint8_t build = 4;
+        uint8_t build = 6;
 
         std::string to_string() const;
         bool operator<(const version_t& that) const;
