1295f3104f47b6e1e52b47327749ff85cf2c4e4b
Xenapte <17661805+Xenapte@users.noreply.github.com>
2023-08-09 16:59:23 +0800

    refactor: use build configurations

--- BallanceMMOClient/bml_includes.h
+++ BallanceMMOClient/bml_includes.h
@@ -3,12 +3,8 @@
 // #define BMMO_USE_BML_PLUS
 #ifdef BMMO_USE_BML_PLUS
 # include <BMLPlus/BMLAll.h>
-# pragma comment(lib, "lib/BMLPlus/BMLPlus.lib")
-# pragma comment(lib, "lib/BMLPlus/CK2.lib")
-# pragma comment(lib, "lib/BMLPlus/VxMath.lib")
   typedef const char* BMMO_CKSTRING;
 #else
 # include <BML/BMLAll.h>
-# pragma comment(lib, "lib/BML/Release/BML.lib")
   typedef CKSTRING BMMO_CKSTRING;
 #endif
