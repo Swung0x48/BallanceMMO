efcaf94c1a5ff258b8c204002957856005fd8dd6
Xenapte <17661805+Xenapte@users.noreply.github.com>
2023-10-01 12:22:17 +0800

    fix: remove picojson from vcpkg dependencies

--- BallanceMMOClient/bml_includes.h
+++ BallanceMMOClient/bml_includes.h
@@ -4,6 +4,8 @@
 #ifdef BMMO_USE_BML_PLUS
 # include <BMLPlus/BMLAll.h>
   typedef const char* BMMO_CKSTRING;
+# define m_bml (m_BML)
+# define m_sprite (m_Sprite)
 #else
 # include <BML/BMLAll.h>
   typedef CKSTRING BMMO_CKSTRING;
