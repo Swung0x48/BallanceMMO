c1ed75dc989e2f399d110ba8b00d4fa77ea225c6
Xenapte <17661805+Xenapte@users.noreply.github.com>
2024-02-26 20:52:11 +0800

    feat: build appimage with ci

--- BallanceMMOServer/start_ballancemmo_loop.sh
+++ BallanceMMOServer/start_ballancemmo_loop.sh
@@ -12,9 +12,19 @@ trap "ctrlc_exit" 2
 for (( ; ; ))
 do
     log_name=$(date +"log_%Y%m%d%H%M.log")
-    chmod +x ./BallanceMMOServer
+    server_binary="./BallanceMMOServer"
+    extra_options=()
+    if [ ! -f "$server_binary" ]; then
+        server_binary=$(ls ./BallanceMMOLaunchSelector*.AppImage | awk '{print $1}')
+        if [ ! -f "$server_binary" ]; then
+            echo "Error: server executable not found." >&2
+            exit 1
+        fi
+        extra_options=("--launch" "Server")
+    fi
+    chmod +x $server_binary
     SECONDS=0
-    ./BallanceMMOServer --log=$log_name "$@"
+    $server_binary "${extra_options[@]}" --log=$log_name "$@"
     server_status=$?
     archive_logs
     if [ $server_status -eq 0 ] || [ $server_status -eq 1 ] || [ $SECONDS -lt 10 ]
